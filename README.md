aef1093864b228b2066205a9e3d1bbb0926ff805
wy <wangyong@chris.local>
2024-03-01 11:17:37 +0800

    初始化工程

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# wy_jscolor
+# 工具库介绍
+
 JavaScript 颜色转换和生成函数
+该 JavaScript 库提供了一系列常用的颜色转换和生成函数。
+
+randomColor(): 生成随机十六进制颜色值。
+randomRGBColor(): 生成随机 RGB 颜色值。
+rgbToHex(r, g, b): 将 RGB 颜色值转换为十六进制字符串。
+hexToRgb(hex): 将十六进制颜色字符串转换为 RGB 颜色值。
+
+
+var color = randomColor();
+console.log("Random Color:", color);
+
+var rgbColor = randomRGBColor();
+console.log("Random RGB Color:", rgbColor);
+
+var hex = rgbToHex(255, 0, 0);
+console.log("RGB to Hex:", hex);
+
+var rgb = hexToRgb("#FF0000");
+console.log("Hex to RGB:", rgb);
+
---
+++ index.js
@@ -0,0 +1,31 @@
+// 生成随机 RGB 颜色
+export function randomRGBColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+// 生成随机颜色 
+export function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// RGB 转换成十六进制字符串
+export function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// 十六进制字符串转换成 RGB
+export function hexToRgb(hex) {
+    var bigint = parseInt(hex.substring(1), 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
