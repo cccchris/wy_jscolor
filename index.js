e3ee280a0d827f498b30761656f51a3eca220b9f
wy <wangyong@chris.local>
2024-03-04 15:07:52 +0800

    添加依赖

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 // 生成随机 RGB 颜色
 export function randomRGBColor() {
     var r = Math.floor(Math.random() * 256);
@@ -28,4 +30,14 @@ export function hexToRgb(hex) {
     var g = (bigint >> 8) & 255;
     var b = bigint & 255;
     return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+// 日期检测 检测当前日期是否在 date 之后
+export function isAfter(currentTime, compareTime) {
+  // 使用 Day.js 解析时间
+  const current = dayjs(currentTime);
+  const compare = dayjs(compareTime);
+  
+  // 判断当前时间是否在比较时间之后
+  return current.isAfter(compare);
 }
